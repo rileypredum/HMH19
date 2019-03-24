9f49849d0b82a3661095116485a1a81c8a06e6c0
Risako Owan <risakoowan@gmail.com>
2019-03-24 08:58:44 -0700

    styling changes

--- mysite/healthfully/admin.py
+++ mysite/healthfully/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from djongo import models
 
 # Register your models here.
+
+# class Emotion(models.Model):
+#     emotion = models.CharField(max_length=100)
+#     listOfAssociatedWords = 
+#     def __str__(self):
+#         return self.name
+
+# class Blog(models.Model):
+#     emotion = models.CharField(max_length=100)
+#     image = models.TextField()
+
+#     def __str__(self):
+#         return self.name
+
+# class Communities(models.Model):
+#     emotion = models.CharField(max_length=100)
+#     tagline = models.TextField()
+
+#     def __str__(self):
+#         return self.name
